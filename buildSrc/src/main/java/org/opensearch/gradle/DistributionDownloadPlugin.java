0762cbaf1411cb731925f5186f0077102296bb4c
Zelin Hao <zelinhao@amazon.com>
2022-11-01 15:48:40 -0700

    Test multi download
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- buildSrc/src/main/java/org/opensearch/gradle/DistributionDownloadPlugin.java
+++ buildSrc/src/main/java/org/opensearch/gradle/DistributionDownloadPlugin.java
@@ -209,10 +209,23 @@ public class DistributionDownloadPlugin implements Plugin<Project> {
 //            new String[]{"/ci/dbc" + CI_SNAPSHOT_PATTERN}:
 //            new String[]{"/releases" + RELEASE_PATTERN_LAYOUT, "/release-candidates" + RELEASE_PATTERN_LAYOUT};
         if (customDistributionUrls != null) {
-            for (String customDistributionUrl : customDistributionUrls.toString().split(",")) {
-                addIvyRepo(project, DOWNLOAD_REPO_NAME, customDistributionUrl, FAKE_IVY_GROUP, "");
-                addIvyRepo(project, SNAPSHOT_REPO_NAME, customDistributionUrl, FAKE_SNAPSHOT_IVY_GROUP, "");
-            }
+            addIvyRepo(project, DOWNLOAD_REPO_NAME, customDistributionUrls.toString(), FAKE_IVY_GROUP, "");
+            addIvyRepo(project, SNAPSHOT_REPO_NAME, customDistributionUrls.toString(), FAKE_SNAPSHOT_IVY_GROUP, "");
+            addIvyRepo(
+                project,
+                DOWNLOAD_REPO_NAME,
+                "https://artifacts.opensearch.org",
+                FAKE_IVY_GROUP,
+                "/releases" + RELEASE_PATTERN_LAYOUT,
+                "/release-candidates" + RELEASE_PATTERN_LAYOUT
+            );
+            addIvyRepo(project, SNAPSHOT_REPO_NAME, "https://artifacts.opensearch.org", FAKE_SNAPSHOT_IVY_GROUP, SNAPSHOT_PATTERN_LAYOUT);
+//            for (String customDistributionUrl : customDistributionUrls.toString().split(",")) {
+////                if (customDistributionUrl.contains(setup_version)) {
+//                    addIvyRepo(project, DOWNLOAD_REPO_NAME, customDistributionUrl, FAKE_IVY_GROUP, "");
+//                    addIvyRepo(project, SNAPSHOT_REPO_NAME, customDistributionUrl, FAKE_SNAPSHOT_IVY_GROUP, "");
+////                }
+//            }
         } else {
             addIvyRepo(
                 project,
